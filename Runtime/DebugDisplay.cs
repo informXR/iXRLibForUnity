0951f54a832f47471e8284453bef4c16bc3839d0
Nick Ramirez <nick@informxr.com>
2024-09-13 14:30:44 -0700

    Allow toggling of DebugWindow

--- Runtime/DebugDisplay.cs
+++ Runtime/DebugDisplay.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DebugDisplay : MonoBehaviour
 {
@@ -8,16 +9,39 @@ public class DebugDisplay : MonoBehaviour
     private const int MaxLines = 15;
 
     private readonly Queue<string> _logQueue = new Queue<string>();
+    
+    public Canvas canvasToToggle;
+    private PlayerInputActions _playerInputActions;
+
+    private void Awake()
+    {
+        // Instantiate the input actions
+        _playerInputActions = new PlayerInputActions();
+    }
 
     private void OnEnable()
     {
         // Subscribe to Unity's log callback
         Application.logMessageReceived += HandleLog;
+        
+        // Enable the action map and listen for the button press
+        _playerInputActions.Enable();
+        _playerInputActions.Newactionmap.ToggleDebugWindow.performed += ToggleCanvas;
     }
 
     private void OnDisable()
     {
         Application.logMessageReceived -= HandleLog;
+        
+        _playerInputActions.Newactionmap.ToggleDebugWindow.performed -= ToggleCanvas;
+        _playerInputActions.Disable();
+    }
+    
+    private void ToggleCanvas(InputAction.CallbackContext context)
+    {
+        Debug.Log("TOGGLE!!!");
+        // Toggle the Canvas visibility
+        canvasToToggle.enabled = !canvasToToggle.enabled;
     }
 
     private void HandleLog(string logString, string stackTrace, LogType type)
