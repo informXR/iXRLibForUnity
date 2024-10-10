0886f01ac009b5b8c0a7727da5324315bb96fd3c
Nick Ramirez <nick@informxr.com>
2024-10-10 15:05:30 -0700

    Make DebugWindow draggable

---
+++ Runtime/DebugWindow/DraggableCanvas.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableCanvas : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    private RectTransform _rectTransform;
+    private float _originalZ;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _originalZ = _rectTransform.position.z;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!eventData.pointerCurrentRaycast.isValid) return;
+        
+        _rectTransform.position = eventData.pointerCurrentRaycast.worldPosition;
+    }
+    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Keep Z position constant
+        Vector3 position = _rectTransform.position;
+        position.z = _originalZ;
+        _rectTransform.position = position;
+        
+        _rectTransform.LookAt(Camera.main.transform);
+        _rectTransform.Rotate(0, 180, 0); // Flip it to face the right direction
+    }
+}
