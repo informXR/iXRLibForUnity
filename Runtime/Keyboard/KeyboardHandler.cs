47a8d5108754612dc039d7feb8ab201be415edfe
Nick Ramirez <nick@informxr.com>
2024-11-07 10:51:35 -0800

    Async Keyboard

--- Runtime/Keyboard/KeyboardHandler.cs
+++ Runtime/Keyboard/KeyboardHandler.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.MixedReality.Toolkit.Experimental.UI;
 using UnityEngine;
 
 public class KeyboardHandler : MonoBehaviour
 {
     private static KeyboardHandler _instance;
+    public static bool ProcessingSubmit;
+    private const string ProcessingText = "Processing";
     
     public static void Initialize()
     {
         if (_instance != null) return;
         
-        var singletonObject = new GameObject("KeyboardPositioner");
+        var singletonObject = new GameObject("KeyboardHandler");
         _instance = singletonObject.AddComponent<KeyboardHandler>();
         DontDestroyOnLoad(singletonObject);
     }
@@ -30,10 +33,27 @@ public class KeyboardHandler : MonoBehaviour
         NonNativeKeyboard.Instance.OnTextSubmitted += HandleTextSubmitted;
     }
 
-    private static void HandleTextSubmitted(object sender, EventArgs e)
+    private async void HandleTextSubmitted(object sender, EventArgs e)
     {
-        NonNativeKeyboard.Instance.Close();
+        if (ProcessingSubmit) return;
+        
+        StartCoroutine(ProcessingVisual());
         var keyboard = (NonNativeKeyboard)sender;
-        Authentication.KeyboardAuthenticate(keyboard.InputField.text);
+        await Authentication.KeyboardAuthenticate(keyboard.InputField.text);
+    }
+    
+    private static IEnumerator ProcessingVisual()
+    {
+        ProcessingSubmit = true;
+        NonNativeKeyboard.Instance.Prompt.text = ProcessingText;
+        while (ProcessingSubmit)
+        {
+            string currentText = NonNativeKeyboard.Instance.Prompt.text;
+            NonNativeKeyboard.Instance.Prompt.text = currentText.Length > ProcessingText.Length + 10 ?
+                ProcessingText :
+                $":{NonNativeKeyboard.Instance.Prompt.text}:";
+            
+            yield return new WaitForSeconds(0.5f); // Wait before running again
+        }
     }
 }
--- Runtime/iXR.cs
+++ Runtime/iXR.cs
@@ -284,6 +284,7 @@ public class iXR
 	// ---
 	public static void PresentKeyboard(string promptText = null, string keyboardType = null, string emailDomain = null)
 	{
+		KeyboardHandler.ProcessingSubmit = false;
 		if (keyboardType is "text" or null)
 		{
 			NonNativeKeyboard.Instance.Prompt.text = promptText ?? "Please Enter Your Login";
