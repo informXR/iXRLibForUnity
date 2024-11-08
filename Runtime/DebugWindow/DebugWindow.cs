87e191557ea41343f761c48a0d43c4fd666adcaf
Nick Ramirez <nick@informxr.com>
2024-11-08 10:21:34 -0800

    Improve Debug Window lag

--- Runtime/DebugWindow/DebugWindow.cs
+++ Runtime/DebugWindow/DebugWindow.cs
@@ -8,7 +8,7 @@ public class DebugWindow : MonoBehaviour
     public TMP_Text logText;
     public ScrollRect scrollRect;
     private readonly List<string> _logs = new();
-    private const int MaxLogs = 50;
+    private const int MaxLogs = 100;
     
     private void OnEnable()
     {
@@ -21,16 +21,13 @@ public class DebugWindow : MonoBehaviour
         Application.logMessageReceived -= HandleLog;
     }
     
-    private void HandleLog(string logString, string stackTrace, LogType type)
+    private async void HandleLog(string logString, string stackTrace, LogType type)
     {
         float roundedTime = Mathf.Round(Time.time * 100f) / 100f;
         string newLog = $"[{roundedTime}] {type}: {logString}";
         _logs.Add(newLog);
         if (_logs.Count > MaxLogs) _logs.RemoveAt(0);
         logText.text = string.Join("\n", _logs);
-        
-        // Force the layout to update immediately
-        LayoutRebuilder.ForceRebuildLayoutImmediate(logText.rectTransform);
 
         // Scroll to the bottom to show the latest text
         scrollRect.verticalNormalizedPosition = 0f;
